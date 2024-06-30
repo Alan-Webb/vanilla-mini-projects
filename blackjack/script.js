635904eee9e7b9a5a246fd90bb7e74521dd15f42
Alan-Webb <webbalan91939@gmail.com>
2024-07-01 00:25:13 +0100

    Work on JS

---
+++ blackjack/script.js
@@ -0,0 +1,18 @@
+let dealerSum = 0;
+let playerSum = 0;
+
+let dealerAceCopunt = 0;
+let playerAceCount = 0;
+
+let hiddenCard;
+let deck;
+
+let canHit = true;
+
+window.onload = function() {
+  buildDeck();
+}
+
+function buildDeck() {
+  let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+}
