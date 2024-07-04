3015bb77ae685c0db453cca60c94a25fe35a810f
Alan-Webb <webbalan91939@gmail.com>
2024-07-04 03:07:14 +0100

    Create populate board functionality

--- JSgames/minesweeper/minesweeper.js
+++ JSgames/minesweeper/minesweeper.js
@@ -18,4 +18,16 @@ window.onload = function() {
 
 function startGame() {
   document.getElementById("mines-count").innerText = minesCount;
+  // populate board
+  for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < cols; c++) {
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      document.getElementById("board").append(tile);
+      row.push(tile);
+    }
+    board.push(row);
+  }
+  console.log(board);
 }
