ffa5c8f52815c6b1ad6861f25b501aff5805c53e
Alan-Webb <webbalan91939@gmail.com>
2024-07-04 03:15:44 +0100

    Make tiles clickable and flag placeable

--- JSgames/minesweeper/minesweeper.js
+++ JSgames/minesweeper/minesweeper.js
@@ -18,16 +18,39 @@ window.onload = function() {
 
 function startGame() {
   document.getElementById("mines-count").innerText = minesCount;
+  document.getElementById("flag-button").addEventListener("click", setFlag);
   // populate board
   for (let r = 0; r < rows; r++) {
     let row = [];
     for (let c = 0; c < cols; c++) {
       let tile = document.createElement("div");
       tile.id = r.toString() + "-" + c.toString();
+      tile.addEventListener("click", clickTile);
       document.getElementById("board").append(tile);
       row.push(tile);
     }
     board.push(row);
   }
   console.log(board);
+}
+
+function setFlag() {
+  if (flagEnabled) {
+    flagEnabled = false;
+    document.getElementById("flag-button").style.backgroundColor = "lightgray";
+  } else {
+    flagEnabled = true;
+    document.getElementById("flag-button").style.backgroundColor = "darkgray";
+  }
+}
+
+function clickTile() {
+  let tile = this;
+  if (flagEnabled) {
+    if (tile.innerText == "") {
+      tile.innerText = "🚩";
+    } else if (tile.innerText == "🚩") {
+      tile.innerText = "";
+    }
+  }
 }
