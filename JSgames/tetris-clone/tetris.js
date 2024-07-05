4d18da043146950cb0b4a1f15d7b9570fef0a8c1
Alan-Webb <webbalan91939@gmail.com>
2024-07-05 14:09:39 +0100

    Draw tetrominoes
    * added drawTetrominoe function

--- JSgames/tetris-clone/tetris.js
+++ JSgames/tetris-clone/tetris.js
@@ -15,34 +15,46 @@ document.addEventListener("DOMContentLoaded", () => {
   ];
 
   const zTetromino = [
-    [0,width,width+1,width*2+1],
-    [width+1, width+2,width*2,width*2+1],
-    [0,width,width+1,width*2+1],
-    [width+1, width+2,width*2,width*2+1]
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0,width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
   ];
 
   const tTetromino = [
-    [1,width,width+1,width+2],
-    [1,width+1,width+2,width*2+1],
-    [width,width+1,width+2,width*2+1],
-    [1,width,width+1,width*2+1]
+    [1, width, width+1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
   ];
 
   const oTetromino = [
-    [0,1,width,width+1],
-    [0,1,width,width+1],
-    [0,1,width,width+1],
-    [0,1,width,width+1]
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
   ];
 
   const iTetromino = [
-    [1,width+1,width*2+1,width*3+1],
-    [width,width+1,width+2,width+3],
-    [1,width+1,width*2+1,width*3+1],
-    [width,width+1,width+2,width+3]
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
   ];
 
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+  let currentPosition = 4;
+  let current = theTetrominoes[0][0];
+
+  function drawTetromino() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add("tetromino")
+    });
+  }
+  drawTetromino();
+
+
 
 
 });
