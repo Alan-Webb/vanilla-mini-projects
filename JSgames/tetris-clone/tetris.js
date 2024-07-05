ee055c5d0d59dbf43bd09ff2c7f031249e139999
Alan-Webb <webbalan91939@gmail.com>
2024-07-05 10:55:18 +0100

    Establish global variables

---
+++ JSgames/tetris-clone/tetris.js
@@ -0,0 +1,9 @@
+const grid = document.querySelector(".grid");
+let squares = Array.from(document.querySelectorAll(".grid div"));
+
+const scoreDisplay = document.querySelector("#score");
+const startBtn = document.querySelector("#start-button");
+const width = 10;
+
+
+console.log(squares);
