f4ec9fad1df0934e2c6dde6183e9a6cddf489a9c
Alan-Webb <webbalan91939@gmail.com>
2024-07-09 18:41:17 +0100

    Create functio to populate board
    * added setGame function to populate board

--- JSgames/connect-4/connect-4.js
+++ JSgames/connect-4/connect-4.js
@@ -4,6 +4,22 @@ let currentPlayer = playerRed;
 
 let gameOver = false;
 
-const board;
+let board;
 const rows = 6;
 const columns = 7;
+
+window.onload = function() {
+  setGame();
+}
+
+function setGame() {
+  board =[];
+  for (let r = 0; r < rows; r++) {
+    let rows = [];
+    for (let c = 0; c < columns; c++) {
+      rows.push("");
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+    }
+  }
+}
