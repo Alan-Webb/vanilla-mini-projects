583aec89f852b0c0cec9bcbcd153c81413cf0087
Alan Webb <webbalan91939@gmail.com>
2024-07-12 18:33:33 +0100

    Add function for setting game pieces
    * added setPiece function

--- JSgames/connect-4/connect-4.js
+++ JSgames/connect-4/connect-4.js
@@ -21,7 +21,28 @@ function setGame() {
       let tile = document.createElement("div");
       tile.id = r.toString() + "-" + c.toString();
       tile.classList.add("tile");
+      tile.addEventListener("click", setPiece);
       document.getElementById("board").append(tile);
     }
   }
+}
+
+function setPiece() {
+  if (gameOver) {
+    return;
+  }
+  let coords = this.id.split("-");
+  let r = parseInt(coords[0]);
+  let c = parseInt(coords[1]);
+
+  board[r][c] = currentPlayer;
+  let tile = document.getElementById(r.toString() + "-" + c.toString());
+    if (currPlayer == playerRed) {
+        tile.classList.add("red-piece");
+        currPlayer = playerYellow;
+    }
+    else {
+        tile.classList.add("yellow-piece");
+        currPlayer = playerRed;
+    }
 }
