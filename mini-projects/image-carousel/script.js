5db6abc3cf8c896c4622c174c370981d237f9455
Alan Webb <webbalan91939@gmail.com>
2024-07-14 21:36:22 +0100

    Reorganise files and tidy code

--- mini-projects/image-carousel/script.js
+++ mini-projects/image-carousel/script.js
@@ -1,40 +1,41 @@
-const imgs = document.getElementById("imgs");
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
 
-const img = document.querySelectorAll("#imgs, img");
+const img = document.querySelectorAll('#imgs img')
 
-let idx = 0;
+let idx = 0
 
-let interval = setInterval(run, 3000);
+let interval = setInterval(run, 2000)
 
 function run() {
-  idx ++;
-  changeImage();
+    idx++
+    changeImage()
 }
 
 function changeImage() {
-  if (idx > img.length -2) {
-    idx = 0;
-  } else if (idx < 0) {
-    idx = img.length -2;
-  }
-  imgs.style.transform = `translateX(${-idx * 500}px)`;
+    if(idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
 }
 
 function resetInterval() {
-  clearInterval(interval);
-  interval = setInterval(run, 3000);
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
 }
 
-nextBtn.addEventListener("click", () => {
-  idx ++;
-  changeImage();
-  resetInterval();
-});
+rightBtn.addEventListener('click', () => {
+    idx++
+    changeImage()
+    resetInterval()
+})
 
-prevBtn.addEventListener("click", () => {
-  idx --;
-  changeImage();
-  resetInterval();
-});
+leftBtn.addEventListener('click', () => {
+    idx--
+    changeImage()
+    resetInterval()
+})
