70590eda009584fc665f48a1d2eddd166ad55aa5
Alan Webb <webbalan91939@gmail.com>
2024-07-14 01:50:53 +0100

    Get elements from DOM
    * added varaibles to script.js to get elements from DOM

---
+++ mini-projects/image-carousel/script.js
@@ -0,0 +1,7 @@
+const imgs = document.getElementById("imgs");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+const imgSelect = document.querySelectorAll("#imgs, img");
+
+
