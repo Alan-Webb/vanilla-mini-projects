aafa65586c5e6ad554f5d574909d58c94ce0be74
Alan Webb <webbalan91939@gmail.com>
2024-07-19 04:41:38 +0100

    Add event listener
    * added eventListener to submit
    
    * created showError function

--- mini-projects/form-validator/script.js
+++ mini-projects/form-validator/script.js
@@ -4,3 +4,20 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+
+//show input error message
+function showError(input, message) {
+	const formControl = input.parentElement;
+	formControl.className = "form-control error";
+}
+
+/* EVENT LISTERNERS */
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
+
+	if (username.value === "") {
+		showError(username, "Username is required");
+	} else {
+		showSuccess(username);
+	}
+});
