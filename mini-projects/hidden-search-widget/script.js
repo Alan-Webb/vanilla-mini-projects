8cee46667b789f4f5e3941f6fbbdcab0797dc39d
Alan Webb <webbalan91939@gmail.com>
2024-07-21 00:40:54 +0100

    Add eventListener for button
    * added eventListener to toggle search input

--- mini-projects/hidden-search-widget/script.js
+++ mini-projects/hidden-search-widget/script.js
@@ -2,3 +2,9 @@
 const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 const input = document.querySelector(".input");
+
+// Event listerners
+btn.addEventListener("click", () => {
+	search.classList.toggle("active");
+	input.focus();
+});
