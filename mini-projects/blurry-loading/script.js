7fe840bdfb223416717b27b7220acbff5b38c6bb
Alan Webb <webbalan91939@gmail.com>
2024-07-22 01:36:36 +0100

    Add fading effect
    * created scale function

--- mini-projects/blurry-loading/script.js
+++ mini-projects/blurry-loading/script.js
@@ -7,8 +7,15 @@ let int = setInterval(blurring, 30);
 
 function blurring() {
 	load++;
-  if(load > 99) {
-    clearInterval(int);
-  }
-	loadText.innerText = `${load}%`
+
+	if (load > 99) {
+		clearInterval(int);
+	}
+
+	loadText.innerText = `${load}%`;
+	loadText.style.opacity = scale(load, 0, 100, 1, 0);
 }
+
+const scale = (num, inMin, inMax, outMin, outMax) => {
+	return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
