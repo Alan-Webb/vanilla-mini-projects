8c713548ca3875b6b5625d364a18e7a3b669d5fd
Alan Webb <webbalan91939@gmail.com>
2024-07-22 02:07:05 +0100

    Complete project
    * added checkboxes function to facilitate effect

--- mini-projects/scroll-animation/script.js
+++ mini-projects/scroll-animation/script.js
@@ -3,3 +3,17 @@ const boxes = document.querySelectorAll(".box");
 
 // Event Listeners
 window.addEventListener("scroll", checkboxes);
+
+function checkboxes() {
+	const triggerBottom = (window.innerHeight / 5) * 4;
+
+	boxes.forEach((box) => {
+		const boxTop = box.getBoundingClientRect().top;
+
+		if (boxTop < triggerBottom) {
+			box.classList.add("show");
+		} else {
+			box.classList.remove("show");
+		}
+	});
+}
