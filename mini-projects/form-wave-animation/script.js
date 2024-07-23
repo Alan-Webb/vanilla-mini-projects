1d23d2c32c9a3e5643a4c9d509b18a2304c9a722
Alan Webb <webbalan91939@gmail.com>
2024-07-24 00:27:39 +0100

    Isolate each letter of label for wave effect
    * used forEach method to array each word and .split and .map to wrap each letter in a span

--- mini-projects/form-wave-animation/script.js
+++ mini-projects/form-wave-animation/script.js
@@ -1,2 +1,10 @@
 // JS representaion from DOM
 const labels = document.querySelectorAll(".form-control label");
+
+// Changes each label into array and each letter into a span
+labels.forEach((label) => {
+	label.innerHTML = label.innerText
+		.split("")
+		.map((letter, idx) => `<span>${letter}</span>`)
+		.join("");
+});
