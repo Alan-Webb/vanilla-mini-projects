73fc465f40c4ebbb5ffc50e63ca97ac2e99011b9
Alan Webb <webbalan91939@gmail.com>
2024-07-25 00:35:09 +0100

    Add eventlistener and fetch request
    * added event listener to joke btn
    
    * created generateJoke function to make fetch request

--- mini-projects/dad-jokes/script.js
+++ mini-projects/dad-jokes/script.js
@@ -1,3 +1,22 @@
 // JS representation from DOM
 const jokeEl = document.getElementById("joke");
 const joekBtn = document.getElementById("jokeBtn");
+
+//Event listeners
+joekBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+	const config = {
+		headers: {
+			Accept: "application/json",
+		},
+	};
+
+	const res = await fetch("https://icanhazdadjoke.com", config);
+
+	const data = await res.json();
+
+	jokeEl.innerHTML = data.joke;
+}
