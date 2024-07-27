13d613ea75abd79a8118fd38b4e74ff8c27f828d
Alan Webb <webbalan91939@gmail.com>
2024-07-27 19:29:39 +0100

    Add eventlister to textArea

--- mini-projects/random-choice-picker/script.js
+++ mini-projects/random-choice-picker/script.js
@@ -1,3 +1,9 @@
 // JS representaion from DOM
 const tagesEl = document.getElementById("tags");
 const textarea = document.getElementById("textArea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+	createTags(e.target.value);
+});
