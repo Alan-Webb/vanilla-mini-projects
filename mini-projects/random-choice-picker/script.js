454444338c6aa3746557be9201e6294910d52c71
Alan Webb <webbalan91939@gmail.com>
2024-07-28 01:57:39 +0100

    Add random selectio functionality
    * added randomSelect and pickRandomTag functions to create node list and randomly select

--- mini-projects/random-choice-picker/script.js
+++ mini-projects/random-choice-picker/script.js
@@ -7,6 +7,12 @@ textarea.focus();
 // Event listeners
 textarea.addEventListener("keyup", (e) => {
 	createTags(e.target.value);
+	if (e.key === "Enter") {
+		setTimeout(() => {
+			e.target.value = "";
+		}, 10);
+		randomSelect();
+	}
 });
 
 function createTags(input) {
@@ -14,5 +20,24 @@ function createTags(input) {
 		.split(",")
 		.filter((tag) => tag.trim() !== "")
 		.map((tag) => tag.trim());
-	console.log(tags);
+	tagesEl.innerHTML = "";
+	tags.forEach((tag) => {
+		const tagEl = document.createElement("span");
+		tagEl.classList.add("tag");
+		tagEl.innerText = tag;
+		tagesEl.appendChild(tagEl);
+	});
+}
+
+function randomSelect() {
+	const times = 30;
+
+	const interval = setInterval(() => {
+		const randomTag = pickRandomTag();
+	}, 100);
+}
+
+function pickRandomTag() {
+	const tags = document.querySelectorAll(".tag");
+	return tags[Math.floor(Math.random() * tags.length)];
 }
