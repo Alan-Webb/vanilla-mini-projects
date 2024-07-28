b8d2cfbe42e6618c2117e297e54a49e28f3df195
Alan Webb <webbalan91939@gmail.com>
2024-07-29 00:13:48 +0100

    Add JS representation from DOM
    * added JS rep from DOM
    
    * Created eventlistener for active class toggle

---
+++ mini-projects/animated-navigation/script.js
@@ -0,0 +1,7 @@
+// JS representaion from DOM
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => {
+	nav.classList.toggle("active");
+});
