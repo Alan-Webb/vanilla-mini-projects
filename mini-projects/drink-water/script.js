00ebc1de2794880bf7841d76d3b2de71afcf7d74
Alan Webb <webbalan91939@gmail.com>
2024-07-31 20:03:29 +0100

    Work on JS
    * created node list of cups using forEach loop
    
    * added eventlistener to cups element that calls highlightCups function
    
    * created highlightCups function

--- mini-projects/drink-water/script.js
+++ mini-projects/drink-water/script.js
@@ -3,3 +3,13 @@ const smallCups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+
+smallCups.forEach((cup, idx) => {
+	cup.addEventListener("click", () => {
+		highlightCups(idx);
+	});
+});
+
+function highlightCups(){
+  
+}
