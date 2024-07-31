2f10ea0994d757e1ce3bba15d3a8e8b5e56675b9
Alan Webb <webbalan91939@gmail.com>
2024-07-31 19:44:06 +0100

    Add JS representaion from DOM

---
+++ mini-projects/drink-water/script.js
@@ -0,0 +1,5 @@
+// JS representation from DOM
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
