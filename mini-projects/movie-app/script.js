5ebedd38eb8f41b12f468be6923c90e4cdca1dd2
Alan Webb <webbalan91939@gmail.com>
2024-08-01 01:48:22 +0100

    Add API links

---
+++ mini-projects/movie-app/script.js
@@ -0,0 +1,6 @@
+const API_URL =
+	"'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=afa86ff9eeaea9be2435de8624d8bb35";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+	"https://api.themoviedb.org/3/search/movie?api_key=afa86ff9eeaea9be2435de8624d8bb35";
+
