b2b52d9c363e57f310a500166e2d61007647e098
Alan Webb <webbalan91939@gmail.com>
2024-08-06 18:22:12 +0100

    Add JS representation from DOM

---
+++ mini-projects/background-slider/script.js
@@ -0,0 +1,5 @@
+// JS representation from DOM
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
