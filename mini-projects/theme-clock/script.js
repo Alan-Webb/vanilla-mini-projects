1bc8936663fb522fe2cf03e80d395a167312d0df
Alan Webb <webbalan91939@gmail.com>
2024-08-07 00:22:12 +0100

    Add dark mode toggle

--- mini-projects/theme-clock/script.js
+++ mini-projects/theme-clock/script.js
@@ -31,3 +31,14 @@ const Months = [
 	"Dec",
 ];
 
+// Dark mode toggle
+toggle.addEventListener("click", (e) => {
+	const html = document.querySelector("html");
+	if (html.classList.contains("dark")) {
+		html.classList.remove("dark");
+		e.target.innerHTML = "Dark Mode";
+	} else {
+		html.classList.add("dark");
+		e.target.innerHTML = "Light Mode";
+	}
+});
