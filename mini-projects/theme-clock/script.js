2b32961429f6162738fe8496829df1f4a68bba82
Alan Webb <webbalan91939@gmail.com>
2024-08-07 00:11:15 +0100

    Add arrays for days and months

--- mini-projects/theme-clock/script.js
+++ mini-projects/theme-clock/script.js
@@ -5,3 +5,29 @@ const secondEl = document.querySelector(".second");
 const timeEl = document.querySelector(".time");
 const dateEl = document.querySelector(".date");
 const toggle = document.querySelector(".toggle");
+
+// Arrays
+const days = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
+const Months = [
+	"Jan",
+	"Feb",
+	"Mar",
+	"Apr",
+	"May",
+	"Jun",
+	"Jul",
+	"Aug",
+	"Sep",
+	"Oct",
+	"Nov",
+	"Dec",
+];
+
