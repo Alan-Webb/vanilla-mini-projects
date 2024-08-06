3b1ae86a74a1ecff81ccf1ba2a2d9edea0d27954
Alan Webb <webbalan91939@gmail.com>
2024-08-07 00:58:53 +0100

    Populate date and digital clock display
    * added conditionals to setTime function

--- mini-projects/theme-clock/script.js
+++ mini-projects/theme-clock/script.js
@@ -16,7 +16,7 @@ const days = [
 	"Friday",
 	"Saturday",
 ];
-const Months = [
+const months = [
 	"Jan",
 	"Feb",
 	"Mar",
@@ -36,49 +36,57 @@ toggle.addEventListener("click", (e) => {
 	const html = document.querySelector("html");
 	if (html.classList.contains("dark")) {
 		html.classList.remove("dark");
-		e.target.innerHTML = "Dark Mode";
+		e.target.innerHTML = "Dark mode";
 	} else {
 		html.classList.add("dark");
-		e.target.innerHTML = "Light Mode";
+		e.target.innerHTML = "Light mode";
 	}
 });
 
-// Populate date / time display
+// Populate date and time display
 function setTime() {
 	const time = new Date();
 	const month = time.getMonth();
 	const day = time.getDay();
+	const date = time.getDate();
 	const hours = time.getHours();
-	const hoursForClock = hours % 12;
+	const hoursForClock = hours >= 13 ? hours % 12 : hours;
 	const minutes = time.getMinutes();
 	const seconds = time.getSeconds();
+	const ampm = hours >= 12 ? "PM" : "AM";
 
 	hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
 		hoursForClock,
 		0,
-		11,
+		12,
 		0,
 		360
-	)})`;
+	)}deg)`;
 	minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
 		minutes,
 		0,
-		59,
+		60,
 		0,
 		360
-	)})`;
+	)}deg)`;
 	secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
 		seconds,
 		0,
-		59,
+		60,
 		0,
 		360
-	)})`;
+	)}deg)`;
+
+	timeEl.innerHTML = `${hoursForClock}:${
+		minutes < 10 ? `0${minutes}` : minutes
+	} ${ampm}`;
+	dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
 }
 
-// maps a range of numbers to another range of numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
 	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
 setTime();
+
 setInterval(setTime, 1000);
