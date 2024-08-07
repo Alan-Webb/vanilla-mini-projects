3871da5bde74a024955bfa6724b4de9153765aa1
Alan Webb <webbalan91939@gmail.com>
2024-08-07 02:07:51 +0100

    Create vars to map coords inside button element

--- mini-projects/button-ripple-effect/script.js
+++ mini-projects/button-ripple-effect/script.js
@@ -1,6 +1,16 @@
 // JS representaion from DOM
 const buttons = document.querySelectorAll(".ripple");
 
+// Maps button coords via click
 buttons.forEach((button) => {
-	button.addEventListener("click", (e) => {});
+	button.addEventListener("click", (e) => {
+		const x = e.clientX;
+		const y = e.clientY;
+
+		const buttonTop = e.target.offsetTop;
+		const buttonLeft = e.target.offsetLeft;
+
+		const xInside = x - buttonLeft;
+		const yInside = y - buttonTop;
+	});
 });
