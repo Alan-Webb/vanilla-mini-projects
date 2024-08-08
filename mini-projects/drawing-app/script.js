ce291093ebcab09ee79e6f328a8c523019894957
Alan Webb <webbalan91939@gmail.com>
2024-08-09 00:45:26 +0100

    Add global vars and create mappinf function
    * added vars for brush size and color
    
    * created drawCircle function

--- mini-projects/drawing-app/script.js
+++ mini-projects/drawing-app/script.js
@@ -3,3 +3,16 @@ const canvas = document.getElementById("canvas");
 
 // Context for canvas
 const ctx = canvas.getContext("2d");
+
+// Global vars
+let size = 20;
+let color = "black";
+
+function drawCircle(x, y) {
+	ctx.beginPath();
+	ctx.arc(x, y, size, 0, Math.PI * 2);
+	ctx.fillStyle = color;
+	ctx.fill();
+}
+
+drawCircle();
