ccc35dbdb2997e92136929a007f36a35902fdf5b
Alan Webb <webbalan91939@gmail.com>
2024-08-09 01:09:46 +0100

    Update JS representaion from DOM for toolbox

--- mini-projects/drawing-app/script.js
+++ mini-projects/drawing-app/script.js
@@ -1,5 +1,10 @@
 // JS representation from DOM
 const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
 
 // Context for canvas
 const ctx = canvas.getContext("2d");
@@ -30,7 +35,12 @@ canvas.addEventListener("mousemove", (e) => {
 	if (isPressed) {
 		const x2 = e.offsetX;
 		const y2 = e.offsetY;
-		console.log(x2, y2);
+
+		drawCircle(x2, y2);
+		drawLine(x, y, x2, y2);
+
+		x = x2;
+		y = y2;
 	}
 });
 
@@ -48,6 +58,6 @@ function drawLine(x1, y1, x2, y2) {
 	ctx.moveTo(x1, y1);
 	ctx.lineTo(x2, y2);
 	ctx.strokeStyle = color;
-	ctx.lineWidth = size;
+	ctx.lineWidth = size * 2;
 	ctx.stroke();
 }
