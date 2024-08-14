f741f559cff1d73eb5a75fc01c606f9d36b0ddf3
Alan Webb <webbalan91939@gmail.com>
2024-08-14 03:48:06 +0100

    Create function to dynamically update navbar
    * added fixNav function

--- mini-projects/sticky-navigation/script.js
+++ mini-projects/sticky-navigation/script.js
@@ -5,3 +5,10 @@ const nav = document.querySelector(".nav");
 window.addEventListener("scroll", fixNav);
 
 // Dynamically updates navbar
+function fixNav() {
+	if (window.scrollY > nav.offsetHeight + 150) {
+		nav.classList.add("active");
+	} else {
+		nav.classList.remove("active");
+	}
+}
