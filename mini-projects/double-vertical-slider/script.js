e8dc98d5e186427e6619b7dbf11c46dbb9c3e5b8
Alan Webb <webbalan91939@gmail.com>
2024-08-15 05:34:08 +0100

    Create slider movement functionality
    * added changeSlide function

--- mini-projects/double-vertical-slider/script.js
+++ mini-projects/double-vertical-slider/script.js
@@ -4,13 +4,27 @@ const slideRight = document.querySelector(".right-slide");
 const slideLeft = document.querySelector(".left-slide");
 const upButton = document.querySelector(".up-button");
 const downButton = document.querySelector(".down-button");
-const slidesLength = slideRight.querySelectorAll("div").length;
+const slideLength = slideRight.querySelectorAll("div").length;
 
 // Global vars
 let activeSlideIndex = 0;
 
-slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+slideLeft.style.top = `-${(slideLength - 1) * 100}vh`;
 
 // Event listeners
 upButton.addEventListener("click", () => changeSlide("up"));
 downButton.addEventListener("click", () => changeSlide("down"));
+
+// Moves slides up and down
+const changeSlide = (direction) => {
+	const sliderHeight = sliderContainer.clientHeight;
+	if (direction === "up") {
+		activeSlideIndex++;
+		if (activeSlideIndex > slideLength - 1) {
+			activeSlideIndex = 0;
+		}
+	}
+	slideRight.style.transform = `translateY(-${
+		activeSlideIndex * sliderHeight
+	}px)`;
+};
