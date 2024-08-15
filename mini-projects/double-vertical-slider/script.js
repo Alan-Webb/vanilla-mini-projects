902c7f36a59a5ea5369077c8d9cd3fffb16ba625
Alan Webb <webbalan91939@gmail.com>
2024-08-15 05:25:49 +0100

    Add global vars

--- mini-projects/double-vertical-slider/script.js
+++ mini-projects/double-vertical-slider/script.js
@@ -4,4 +4,9 @@ const slideRight = document.querySelector(".right-slide");
 const slideLeft = document.querySelector(".left-slide");
 const upButton = document.querySelector(".up-button");
 const downButton = document.querySelector(".down-button");
-const slidesLength = slideRight.querySelectorAll("div");
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+// Global vars
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
