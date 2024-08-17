83b107023d9e73fdb97f0bfc5aaf69c773a0233f
Alan Webb <webbalan91939@gmail.com>
2024-08-17 06:43:11 +0100

    Create add event listener to button

--- mini-projects/toast-notification/script.js
+++ mini-projects/toast-notification/script.js
@@ -1,3 +1,13 @@
 // JS representation from DOM
 const button = document.getElementById("button");
 const toasts = document.getElementById("toasts");
+
+const messages = [
+  "Message One",
+  "Message Two",
+  "Message Three",
+  "Message Four",
+]
+
+// Event listeners
+button.addEventListener("click", ()=> createNotification())
