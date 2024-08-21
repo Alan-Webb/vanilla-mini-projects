96e978e9a1b0bc0ce6184b4cf7d98199bff362be
Alan Webb <webbalan91939@gmail.com>
2024-08-21 11:14:56 +0100

    Add createHeart function to generate heart

--- mini-projects/double-click-heart/script.js
+++ mini-projects/double-click-heart/script.js
@@ -18,3 +18,24 @@ loveMe.addEventListener("click", (e) => {
 		}
 	}
 });
+
+// Creates heart image on dbl click
+const createHeart = (e) => {
+	const heart = document.createElement("i");
+	heart.classList.add("fas");
+	heart.classList.add("fa-heart");
+
+	const x = e.clientX;
+	const y = e.clientY;
+
+	const leftOffset = e.target.offsetLeft;
+	const topOffset = e.target.offsetTop;
+
+	const xInside = x - leftOffset;
+	const yInside = y - topOffset;
+
+	heart.style.top = `${yInside}px`;
+	heart.style.left = `${xInside}px`;
+
+	loveMe.appendChild(heart);
+};
