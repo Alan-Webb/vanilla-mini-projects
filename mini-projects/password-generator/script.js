36a4dbd3f5d097b4006d77e4487ff8955a61909f
Alan Webb <webbalan91939@gmail.com>
2024-08-23 15:00:46 +0100

    Create JS representation from DOM
    * added event listerner to generateEl
    
    * created generatePassword function

--- mini-projects/password-generator/script.js
+++ mini-projects/password-generator/script.js
@@ -1,9 +1,44 @@
+// JS representaion from DOM
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
 const randomFunc = {
 	lower: getRandomLower,
 	upper: getRandomUpper,
 	number: getRandomNumber,
 	symbol: getRandomSymbol,
 };
+
+// Event listeners
+generateEl.addEventListener("click", () => {
+	const length = +lengthEl.value;
+	const hasLower = lowercaseEl.checked;
+	const hasUpper = uppercaseEl.checked;
+	const hasNumber = numbersEl.checked;
+	const hasSymbol = symbolsEl.checked;
+
+	resultEl.innerText = generatePassword(
+		hasLower,
+		hasUpper,
+		hasNumber,
+		hasSymbol,
+		length
+	);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatedPassword = "";
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(
+		(item) => Object.values(item)[0]
+	);
+}
 
 // Generates random lowercase character
 function getRandomLower() {
@@ -25,4 +60,3 @@ function getRandomSymbol() {
 	const symbols = "!@#$%^&*(){}[]=<>/,.";
 	return symbols[Math.floor(Math.random() * symbols.length)];
 }
-console.log(getRandomSymbol());
