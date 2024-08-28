9a88ffe09ad8e36e9e8655d675ecf5fd76e0d7fe
Alan Webb <webbalan91939@gmail.com>
2024-08-28 12:40:46 +0100

    Add local storage functionality

--- mini-projects/notes-app/script.js
+++ mini-projects/notes-app/script.js
@@ -1,7 +1,11 @@
-// JS representation from DOM
 const addBtn = document.getElementById("add");
 
-// Event listeners
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+	notes.forEach((note) => addNewNote(note));
+}
+
 addBtn.addEventListener("click", () => addNewNote());
 
 function addNewNote(text = "") {
@@ -9,37 +13,51 @@ function addNewNote(text = "") {
 	note.classList.add("note");
 
 	note.innerHTML = `
-      <div class="tools">
-				<button class="edit">
-					<i class="fas fa-edit"></i>
-				</button>
-				<button class="delete">
-					<i class="fas fa-trash-alt"></i>
-				</button>
-			</div>
-			<div class="main ${text ? "" : "hidden"}"></div>
-			<textarea class="${text ? "hidden" : ""}"></textarea>`;
+    <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+
+    <div class="main ${text ? "" : "hidden"}"></div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>
+    `;
 
 	const editBtn = note.querySelector(".edit");
 	const deleteBtn = note.querySelector(".delete");
-	const mainBtn = note.querySelector(".main");
+	const main = note.querySelector(".main");
 	const textArea = note.querySelector("textarea");
 
 	textArea.value = text;
-	mainBtn.innerHTML = marked(text);
+	main.innerHTML = marked(text);
 
 	deleteBtn.addEventListener("click", () => {
 		note.remove();
+
+		updateLS();
 	});
 
 	editBtn.addEventListener("click", () => {
-		mainBtn.classList.toggle("hidden");
+		main.classList.toggle("hidden");
 		textArea.classList.toggle("hidden");
 	});
 
 	textArea.addEventListener("input", (e) => {
-		const {value} = (e.targetmain.innerHTML = marked(value));
+		const {value} = e.target;
+
+		main.innerHTML = marked(value);
+
+		updateLS();
 	});
 
 	document.body.appendChild(note);
+}
+
+function updateLS() {
+	const notesText = document.querySelectorAll("textarea");
+
+	const notes = [];
+
+	notesText.forEach((note) => notes.push(note.value));
+
+	localStorage.setItem("notes", JSON.stringify(notes));
 }
