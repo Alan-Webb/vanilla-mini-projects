6b205447d15beaa2fc93b108977d4f0ccd9ad900
Alan Webb <webbalan91939@gmail.com>
2024-08-29 12:39:54 +0100

    Add JS representation from DOM

---
+++ mini-projects/animated-countdown/script.js
@@ -0,0 +1,7 @@
+// JS representation from DOM
+const nums = document.querySelectorAll(".nums span");
+const counter = document.querySelector(".counter");
+const finalMessage = document.querySelector(".final");
+const replay = document.querySelector("#replay");
+
+
