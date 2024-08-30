22021385791aa3b825dfd88ce62b2b2875d55e93
Alan Webb <webbalan91939@gmail.com>
2024-08-30 12:48:02 +0100

    Add mouse functionality

--- mini-projects/hoverboard/script.js
+++ mini-projects/hoverboard/script.js
@@ -4,9 +4,13 @@ const container = document.getElementById("container");
 const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
 const SQUARES = 500;
 
+// Mouse functionality and div generation
 for (let i = 0; i < SQUARES; i++) {
 	const square = document.createElement("div");
 	square.classList.add("square");
+
+	square.addEventListener("mouseover", () => setColor(square));
+	square.addEventListener("mouseout", () => removeColor(square));
 
 	container.appendChild(square);
 }
