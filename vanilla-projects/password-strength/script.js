1263dbb7cf388d6e3fd074ad37e2271750699e7e
Alan Webb <webbalan91939@gmail.com>
2024-09-04 11:12:39 +0100

    Add event listener to password id

--- vanilla-projects/password-strength/script.js
+++ vanilla-projects/password-strength/script.js
@@ -1,3 +1,9 @@
 // JS representation from DOM
 const password = document.getElementById("password");
 const background = document.getElementById("background");
+
+// Event listeners
+password.addEventListener("input", (e)=>{
+  const input = e.target.value;
+  console.log(input);
+})
