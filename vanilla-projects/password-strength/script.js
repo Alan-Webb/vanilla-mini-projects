29836aa0e69752debdb827ccd8d14a63db061b87
Alan Webb <webbalan91939@gmail.com>
2024-09-04 11:16:48 +0100

    Update event listener to decrease image blur

--- vanilla-projects/password-strength/script.js
+++ vanilla-projects/password-strength/script.js
@@ -3,7 +3,9 @@ const password = document.getElementById("password");
 const background = document.getElementById("background");
 
 // Event listeners
-password.addEventListener("input", (e)=>{
-  const input = e.target.value;
-  console.log(input);
-})
+password.addEventListener("input", (e) => {
+	const input = e.target.value;
+	const length = input.length;
+	const blurValue = 20 - length * 2;
+	background.style.filter = `blur(${blurValue}px)`;
+});
