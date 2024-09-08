01b89248004d36b720f809ad097a622574101a7f
Alan-Webb <webbalan91939@gmail.com>
2024-09-08 01:29:04 +0100

    Add getData function to fetch api data

--- vanilla-projects/live-user-filter/script.js
+++ vanilla-projects/live-user-filter/script.js
@@ -1,3 +1,15 @@
 // JS representation from DOM
 const result = document.getElementById("result");
 const filter = document.getElementById("filter");
+
+const listItems = [];
+
+getData();
+
+async function getData() {
+	const res = await fetch("https://randomuser.me/api?results=50");
+
+	const data = await res.json();
+
+	results.innerHTML
+}
