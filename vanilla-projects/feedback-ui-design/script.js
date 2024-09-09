ec1b4de304620a8e5bab00de8e477416b75403f3
Alan Webb <webbalan91939@gmail.com>
2024-09-09 01:31:40 +0100

    Add event listener to panel element

--- vanilla-projects/feedback-ui-design/script.js
+++ vanilla-projects/feedback-ui-design/script.js
@@ -2,3 +2,10 @@
 const ratings = document.querySelectorAll(".rating");
 const sendBtn = document.querySelector("#send");
 const panel = document.querySelector("#panel");
+
+// Event listeners
+panel.addEventListener("click", (e) => {
+	if (e.target.classList.contains("rating")) {
+		console.log(e.target);
+	}
+});
