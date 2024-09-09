726ba7a31197d7d41c563719033276981b529fd4
Alan Webb <webbalan91939@gmail.com>
2024-09-09 01:21:36 +0100

    Add JS representation from DOM

---
+++ vanilla-projects/feedback-ui-design/script.js
@@ -0,0 +1,4 @@
+// JS representaion from DOM
+const ratings = document.querySelectorAll(".rating");
+const sendBtn = document.querySelector("#send");
+const panel = document.querySelector("#panel");
