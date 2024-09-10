217329e78272aae0bb6ba71760e7db298b835500
Alan Webb <webbalan91939@gmail.com>
2024-09-10 19:42:34 +0100

    Add JS representation from DOM

---
+++ vanilla-projects/sidebar/script.js
@@ -0,0 +1,6 @@
+// JS representation from DOM
+const dark = document.querySelector(".dark");
+const night = document.querySelector(".night");
+const light = document.querySelector(".light");
+const sidebar = document.querySelector(".sidebar");
+const activeListItem = document.querySelector(".list-item.active");
