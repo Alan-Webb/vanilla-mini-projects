d0433f6269b5e2e5e20b715aa8760d5dce503d20
Alan Webb <webbalan91939@gmail.com>
2024-09-13 19:57:25 +0100

    Add event listener to playBtn
    * added logic to toggle classes for play / pause

--- vanilla-projects/simple-timer/script.js
+++ vanilla-projects/simple-timer/script.js
@@ -10,6 +10,17 @@ let playing = false;
 let currentSeconds = totalSeconds;
 timerEl.innerText = formatTime(totalSeconds);
 
+// Event listeners
+playBtn.addEventListener("click", () => {
+	playing = !playing;
+	playBtn.classList.toggle("play");
+	playBtn.classList.toggle("bg-green-500");
+
+	const playIcon = playBtn.querySelector("i");
+	playIcon.classList.toggle("fa-play");
+	playIcon.classList.toggle("fa-pause");
+});
+
 // Format time
 function formatTime(seconds) {
 	const minutes = Math.floor(seconds / 60);
