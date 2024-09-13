5193ce5e41a587ea704c11fc2ef8682b276ab1b5
Alan Webb <webbalan91939@gmail.com>
2024-09-13 20:11:26 +0100

    Create resetAll function

--- vanilla-projects/simple-timer/script.js
+++ vanilla-projects/simple-timer/script.js
@@ -1,43 +1,39 @@
 // JS representation from DOM
-const resetBtn = document.getElementById("reset");
-const playBtn = document.getElementById("play");
-const timerEl = document.getElementById("timer");
+const resetBtn = document.querySelector("#reset");
+const playBtn = document.querySelector("#play");
+const timerEl = document.querySelector("#timer");
 const root = document.querySelector(":root");
 
-// Initial set up
+// Initial setup
 const totalSeconds = 60;
 let playing = false;
 let currentSeconds = totalSeconds;
 timerEl.innerText = formatTime(totalSeconds);
 
-const timerInterval = setInterval(runTimer, 1000);
+const timerInterval = setInterval(run, 1000);
 
-// Event listeners
 playBtn.addEventListener("click", () => {
 	playing = !playing;
 	playBtn.classList.toggle("play");
-	playBtn.classList.toggle("bg-green-500");
-
+	playBtn.classList.toggle("bg-green-500"); // Toggle the color class
 	const playIcon = playBtn.querySelector("i");
-	playIcon.classList.toggle("fa-play");
-	playIcon.classList.toggle("fa-pause");
+	playIcon.classList.toggle("fa-play"); // Toggle the play icon
+	playIcon.classList.toggle("fa-pause"); // Toggle the pause icon
 });
+resetBtn.addEventListener("click", resetAll);
 
 // Run timer
-function runTimer() {
+function run() {
 	if (playing) {
 		currentSeconds -= 1;
 		if (currentSeconds <= 0) {
 			clearInterval(timerInterval);
+			resetAll();
 		}
+
 		timerEl.innerText = formatTime(currentSeconds);
 		root.style.setProperty("--degrees", calcDeg());
 	}
-}
-
-// Calculate degrees
-function calcDeg() {
-	return `${360 - (currentSeconds / totalSeconds) * 360}deg`;
 }
 
 // Format time
@@ -48,4 +44,24 @@ function formatTime(seconds) {
 	return `${minutes.toString().padStart(2, "0")}:${newSeconds
 		.toString()
 		.padStart(2, "0")}`;
+}
+
+// Calculate degrees
+function calcDeg() {
+	return `${360 - (currentSeconds / totalSeconds) * 360}deg`;
+}
+
+// Reset values
+function resetAll() {
+	playing = false;
+	playBtn.classList.remove("play");
+	playBtn.classList.remove("bg-green-500");
+
+	const playIcon = playBtn.querySelector("i");
+	playIcon.classList.remove("fa-pause");
+	playIcon.classList.add("fa-play");
+
+	currentSeconds = totalSeconds;
+	timerEl.innerText = formatTime(totalSeconds);
+	root.style.setProperty("--degrees", "0deg");
 }
