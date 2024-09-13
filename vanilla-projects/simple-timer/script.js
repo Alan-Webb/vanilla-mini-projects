2fb0a4958b01ccf7c43c2d82a0ce5575918ba76f
Alan Webb <webbalan91939@gmail.com>
2024-09-13 19:32:11 +0100

    Add JS representation from DOM

---
+++ vanilla-projects/simple-timer/script.js
@@ -0,0 +1,5 @@
+// JS representation from DOM
+const resetBtn = document.getElementById("reset");
+const playBtn = document.getElementById("play");
+const timerEl = document.getElementById("timer");
+const root = document.querySelector(":root");
