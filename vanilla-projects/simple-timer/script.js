c0a69d82edb073556e485c309b5a23b8b1b1bfec
Alan Webb <webbalan91939@gmail.com>
2024-09-13 19:37:43 +0100

    Establish globals vars & create formatTime func

--- vanilla-projects/simple-timer/script.js
+++ vanilla-projects/simple-timer/script.js
@@ -3,3 +3,14 @@ const resetBtn = document.getElementById("reset");
 const playBtn = document.getElementById("play");
 const timerEl = document.getElementById("timer");
 const root = document.querySelector(":root");
+
+// Initial set up
+const totalSeconds = 60;
+let playing = false;
+let currentSeconds = totalSeconds;
+timerEl.innerText = formatTime(totalSeconds);
+
+// Format time
+function formatTime(seconds) {
+  
+}
