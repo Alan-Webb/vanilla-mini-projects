ab225a9a039f2762f5437d06dd819f1db72c3e33
Alan Webb <webbalan91939@gmail.com>
2024-09-16 01:38:16 +0100

    Add JS representation from DOM

---
+++ vanilla-projects/testimonial-box-switcher/script.js
@@ -0,0 +1,3 @@
+const testimonialsContainer = document.querySelector(".testimonial-container");
+const testimonial = document.querySelector(".testimonial");
+const userImage = document.querySelector(".user-img");
