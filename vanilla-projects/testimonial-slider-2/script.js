2a406083ada3939859ffbd520766722b2e742d51
Alan Webb <webbalan91939@gmail.com>
2024-09-18 01:11:36 +0100

    Add event listeners to buttons

--- vanilla-projects/testimonial-slider-2/script.js
+++ vanilla-projects/testimonial-slider-2/script.js
@@ -1,3 +1,15 @@
+// JS representation from DOM
 const container = document.querySelector(".testimonial-slider");
-const prevBtn = document.querySelector(".prev")
-const nextBtn = document.querySelector(".next")
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+// Event listeners
+nextBtn.addEventListener("click", () => {
+	const slides = document.querySelectorAll(".slide");
+	container.appendChild(slides[0]);
+});
+
+prevBtn.addEventListener("click", () => {
+	const slides = document.querySelectorAll(".slide");
+	container.removeChild(slides.length - 1);
+});
