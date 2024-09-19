b025c016ec8882fa9826dad2db783deb0f292a0f
Alan Webb <webbalan91939@gmail.com>
2024-09-19 18:54:53 +0100

    Add event listener to form

--- vanilla-projects/todo-list/script.js
+++ vanilla-projects/todo-list/script.js
@@ -2,3 +2,10 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
+
+// Event listeners
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	addTodd();
+});
