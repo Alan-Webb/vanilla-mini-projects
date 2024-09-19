8f089063d08fccf3fde704545fc2e4e86a4da2ac
Alan Webb <webbalan91939@gmail.com>
2024-09-19 18:58:23 +0100

    Create addTodo function

--- vanilla-projects/todo-list/script.js
+++ vanilla-projects/todo-list/script.js
@@ -1,11 +1,20 @@
-// JS representation from DOM
+// JS representation from DOM (1)
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
 
-// Event listeners
+// Event listeners (2)
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	addTodd();
 });
+
+// Adds todo to list (3)
+function addTodo(todo) {
+	let todoText = input.value;
+
+	if (todo) {
+		todoText = todo.text;
+	}
+}
