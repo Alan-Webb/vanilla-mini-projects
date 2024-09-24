f3106dec6dc8bb71ac2ded21da3955dbb32894db
Alan Webb <webbalan91939@gmail.com>
2024-09-24 02:16:04 +0100

    Update JS logic

--- vanilla-projects/autocomplete/script.js
+++ vanilla-projects/autocomplete/script.js
@@ -22,3 +22,44 @@ const searchWrapper = document.querySelector(".search-input");
 const inputBox = document.querySelector(".input-field");
 const autoBox = document.querySelector(".autobox");
 const btn = document.querySelector(".btn");
+const linkEl = searchWrapper.querySelector("a");
+
+let GoogleLink;
+
+// Event listeners
+inputBox.addEventListener("keyup", (e) => {
+	let userData = e.target.value;
+	let emptyArray = [];
+
+	if (userData) {
+		btn.addEventListener("click", () => {
+			GoogleLink = `https://www.google.com/search?q=${userData}`;
+			linkEl.setAttribute("href", GoogleLink);
+			linkEl.click();
+		});
+
+		emptyArray = data.filter((para) => {
+			return para.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+		});
+
+		emptyArray = emptyArray.map((liEl) => {
+			return (liEl = "<li>" + liEl + "</li>");
+		});
+
+		searchWrapper.classList.add("active");
+		showSuggestion(emptyArray);
+	} else {
+	}
+});
+
+function showSuggestion(list) {
+	let listData;
+
+	if (!list.length) {
+		let userValue = inputBox.value;
+		listData = `<li> ${userValue} </li>`;
+	} else {
+		listData = list.join(" ");
+	}
+	autoBox.innerHTML = listData;
+}
