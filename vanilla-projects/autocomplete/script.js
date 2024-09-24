598db406a3888c8313c4f6ac378b59bd24415faf
Alan Webb <webbalan91939@gmail.com>
2024-09-24 01:43:16 +0100

    Complete CSS styling

---
+++ vanilla-projects/autocomplete/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const data = [
+	"Designer",
+	"Developer",
+	"Web designer",
+	"Web Developer",
+	"Login Form in HTML & CSS",
+	"How to learn HTML & CSS",
+	"How to become a Freelancer",
+	"How to become a Web Designer",
+	"How to start a Gaming Channel",
+	"How to start a YouTube Channel",
+	"Whta does HTML stand for?",
+	"What does CSS stand for?",
+	"Python",
+	"Udemy",
+];
+
+const
