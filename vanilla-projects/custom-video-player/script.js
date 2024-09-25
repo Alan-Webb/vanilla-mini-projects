1178f826193dcf4d68fcd857f74d70eb8cd9ee7b
Alan Webb <webbalan91939@gmail.com>
2024-09-25 19:41:24 +0100

    Add event listeners and define functions

--- vanilla-projects/custom-video-player/script.js
+++ vanilla-projects/custom-video-player/script.js
@@ -4,3 +4,23 @@ const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const progress = document.getElementById("progress");
 const timestamp = document.getElementById("timestamp");
+
+// Event listeners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+progress.addEventListener("change", setVideoProgress);
+
+function toggleVideoStatus() {}
+
+function updatePlayIcon() {}
+
+function updateProgress() {}
+
+function stopVideo() {}
+
+function setVideoProgress() {}
