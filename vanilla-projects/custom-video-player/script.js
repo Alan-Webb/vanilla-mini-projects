0cdf5d684babad891e3fa3aa33a5c07100efe5b5
Alan Webb <webbalan91939@gmail.com>
2024-09-25 19:03:19 +0100

    Add JS representation from DOM & video file

---
+++ vanilla-projects/custom-video-player/script.js
@@ -0,0 +1,6 @@
+// JS representation from DOM
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
