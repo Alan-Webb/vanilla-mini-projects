3332e7727aff757f304dc808660678b66faa2055
Alan Webb <webbalan91939@gmail.com>
2024-10-11 20:33:17 +0100

    Create getRandomMeal function

--- vanilla-projects/meal-finder/script.js
+++ vanilla-projects/meal-finder/script.js
@@ -59,5 +59,36 @@ function getMealById(mealID) {
 		});
 }
 
+// Fetch random meal from API
+function getRandomMeal() {
+	// Clear meals and heading
+	mealsEl.innerHTML = "";
+	resultHeading.innerHTML = "";
+
+	fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+		.then((res) => res.json())
+		.then((data) => {
+			const meal = data.meals[0];
+
+			addMealToDOM(meal);
+		});
+}
+
 // Event listeners
 submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
+
+mealsEl.addEventListener("click", (e) => {
+	const mealInfo = e.composedPath().find((item) => {
+		if (item.classList) {
+			return item.classList.contains("meal-info");
+		} else {
+			return false;
+		}
+	});
+
+	if (mealInfo) {
+		const mealID = mealInfo.getAttribute("data-mealid");
+		getMealById(mealID);
+	}
+});
