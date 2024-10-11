76d533f1d5b1143bc610dd10eaedd84ffc9f67ca
Alan Webb <webbalan91939@gmail.com>
2024-10-11 20:14:43 +0100

    Update searchMeal function

--- vanilla-projects/meal-finder/script.js
+++ vanilla-projects/meal-finder/script.js
@@ -3,7 +3,7 @@ const search = document.getElementById("search");
 const submit = document.getElementById("submit");
 const random = document.getElementById("random");
 const mealsEl = document.getElementById("meals");
-const resultHeading = document.getElementById("result-heading");
+const resultHeading = document.getElementById("results-heading");
 const single_mealEl = document.getElementById("single-meal");
 
 // Search meal and fetch from API
@@ -22,6 +22,24 @@ function searchMeal(e) {
 			.then((res) => res.json())
 			.then((data) => {
 				console.log(data);
+				resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+				if (data.meals === null) {
+					resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+				} else {
+					mealsEl.innerHTML = data.meals
+						.map(
+							(meal) => `
+            <div class="meal">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+              <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+              </div>
+            </div>
+            `
+						)
+						.join("");
+				}
 			});
 	} else {
 		alert("Please enter a search term");
