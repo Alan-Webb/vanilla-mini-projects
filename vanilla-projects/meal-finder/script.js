73bcec91b121d153fb466a5e5b72b99f6b4f4aba
Alan Webb <webbalan91939@gmail.com>
2024-10-11 20:22:05 +0100

    Complete CSS styling for .meals class

--- vanilla-projects/meal-finder/script.js
+++ vanilla-projects/meal-finder/script.js
@@ -25,22 +25,24 @@ function searchMeal(e) {
 				resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
 
 				if (data.meals === null) {
-					resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+					resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
 				} else {
 					mealsEl.innerHTML = data.meals
 						.map(
 							(meal) => `
             <div class="meal">
-              <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
               <div class="meal-info" data-mealID="${meal.idMeal}">
                 <h3>${meal.strMeal}</h3>
               </div>
             </div>
-            `
+          `
 						)
 						.join("");
 				}
 			});
+		// Clear search text
+		search.value = "";
 	} else {
 		alert("Please enter a search term");
 	}
