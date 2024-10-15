2d7768174c87e660e7795cc168dc0829821c3d48
Alan Webb <webbalan91939@gmail.com>
2024-10-15 03:02:19 +0100

    Update addTransactionDOM function

--- vanilla-projects/expense-tracker/script.js
+++ vanilla-projects/expense-tracker/script.js
@@ -24,7 +24,12 @@ function addTransactionDOM(transaction) {
 
 	// Add class based on value
 	item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
 	item.innerHTML = `
-  ${transaction.text} <span>${sign}${transaction.amount}</span>
+  ${transaction.text} <span>${sign}${Math.abs(
+		transaction.amount
+	)}</span><button class="delete-btn">X</button>
   `;
+
+	list.appendChild(item);
 }
