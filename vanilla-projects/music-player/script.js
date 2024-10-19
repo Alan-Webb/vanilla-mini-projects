35ba6ea32285ca99408898e74a38bc5b3c972a7c
Alan Webb <webbalan91939@gmail.com>
2024-10-19 16:53:43 +0100

    Create pauseSong function

--- vanilla-projects/music-player/script.js
+++ vanilla-projects/music-player/script.js
@@ -30,6 +30,17 @@ function playSong() {
 	musicContainer.classList.add("play");
 	playBtn.querySelector("i.fas").classList.remove("fa-play");
 	playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+	audio.play();
+}
+
+// Pause song
+function pauseSong() {
+	musicContainer.classList.remove("play");
+	playBtn.querySelector("i.fas").classList.add("fa-play");
+	playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+	audio.pause();
 }
 
 // Event listeners
