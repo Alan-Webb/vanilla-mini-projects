31721c03af516cd0281b350b24e3af88b893c3e9
Alan Webb <webbalan91939@gmail.com>
2024-10-19 19:49:53 +0100

    Create setProgress function

--- vanilla-projects/music-player/script.js
+++ vanilla-projects/music-player/script.js
@@ -72,6 +72,15 @@ function updateProgress(e) {
 	progress.style.width = `${progressPercent}%`;
 }
 
+// Set progress bar
+function setProgress(e) {
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = audio.duration;
+
+	audio.currentTime = (clickX / width) * duration;
+}
+
 // Event listeners
 playBtn.addEventListener("click", () => {
 	const isPlaying = musicContainer.classList.contains("play");
@@ -86,3 +95,5 @@ playBtn.addEventListener("click", () => {
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
 audio.addEventListener("timeupdate", updateProgress);
+progressContainer.addEventListener("click", setProgress);
+audio.addEventListener("ended", nextSong);
