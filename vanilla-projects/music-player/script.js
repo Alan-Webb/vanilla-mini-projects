4190b27beeef41e669c7decc46dc748d02eaa069
Alan Webb <webbalan91939@gmail.com>
2024-10-19 16:43:49 +0100

    Create loadSong function

--- vanilla-projects/music-player/script.js
+++ vanilla-projects/music-player/script.js
@@ -1,10 +1,24 @@
 // JS representation from DOM
-const musicContainer = document.getElementById("music_container");
+const musicContainer = document.getElementById("music-container");
 const playBtn = document.getElementById("play");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 const audio = document.getElementById("audio");
 const progress = document.getElementById("progress");
-const progressContainer = document.getElementById("progress_container");
+const progressContainer = document.getElementById("progress-container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
+
+// Song titles
+const songs = ["hey", "summer", "ukulele"];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+	title.innerText = song;
+}
