55ed838baea6732a1c22b06fba3985b7f2404c9a
Alan Webb <webbalan91939@gmail.com>
2024-11-01 19:37:58 +0000

    Create getPosts function to fethc posts from API

--- vanilla-projects/infinite-scroll-posts/script.js
+++ vanilla-projects/infinite-scroll-posts/script.js
@@ -5,3 +5,14 @@ const filter = document.getElementById("filter");
 
 let limit = 5;
 let page = 1;
+
+// Fetch posts from API
+async function getPosts() {
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+	);
+
+	const data = await res.json();
+
+	return data;
+}
