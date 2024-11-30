e39693b935edc91be2095118bc9d7d143bbc42db
Alan Webb <webbalan91939@gmail.com>
2024-11-30 18:58:21 +0000

    Create searchSongs function

--- vanilla-projects/lyrics-search-app/script.js
+++ vanilla-projects/lyrics-search-app/script.js
@@ -6,6 +6,14 @@ const more = document.getElementById("more");
 
 const apiURL = "https://api.lyrics.ovh";
 
+// Search by song or artist
+async function searchSongs(term) {
+	const res = await fetch(`${apiURL}/suggest/${term}`);
+	const data = await res.json();
+
+	showData(data);
+}
+
 // Event listeners
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
