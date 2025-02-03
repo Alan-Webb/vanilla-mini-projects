339d2fe70361cde5b6135cfe7520a8fef4dc8d51
Alan Webb <webbalan91939@gmail.com>
2025-02-03 06:40:46 +0000

    Update JS logic

--- speak-number-game/script.js
+++ speak-number-game/script.js
@@ -30,4 +30,30 @@ function writeMessage(msg) {
 
 // Check msg against number
 function checkNumber(msg) {
-  const num = +msg;
+	const num = +msg;
+
+	// Check if valid number
+	if (Number.isNaN(num)) {
+		msgEl.innerHTML += "<div>That is not a valid number</div>";
+		return;
+	}
+
+	// Check in range
+	if (num > 100 || num < 1) {
+		msgEl.innerHTML += "<div>Number must be between 1 and 100</div>";
+		return;
+	}
+
+	// Check number
+	if (num === randomNum) {
+		document.body.innerHTML = `
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+	} else if (num > randomNum) {
+		msgEl.innerHTML += "<div>GO LOWER</div>";
+	} else {
+		msgEl.innerHTML += "<div>GO HIGHER</div>";
+	}
+}
